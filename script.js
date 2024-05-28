0707489a186a8ce32f3d9fb3a6ac3c943e2ed399
Samet Bayram <sametbayramofficial@gmail.com>
2024-05-28 22:48:51 +0300

    JS Learning Day 1

---
+++ script.js
@@ -0,0 +1,36 @@
+let js = "amazing";
+//if(js === "amazing") alert("JavaScript is FUN!")
+console.log(40+23+12-34);
+
+console.log('Samet');
+console.log(24);
+
+let firstName = "Samet";
+let lastName = "Bayram";
+let age = 24;
+let birthYear = 1999;
+
+console.log(firstName);
+console.log(firstName+" "+lastName);
+console.log(age+birthYear);
+console.log(2024-birthYear);
+
+//let 3years = 3; This code has a syntax error.
+// & tanımlarda kullanılamaz.
+//reserved words doesn't useable after let
+
+let PI = 3.141596;
+//This section is more understandable
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teachler';
+// This section is not understandable yet.
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+let country = 'Turkiye';
+let continent = 'Asia';
+let population = 85000000;
+
+console.log(country+" "+continent+" "+population);
